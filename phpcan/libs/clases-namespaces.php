77617438f6d2ef2b5d46e5b534b9e07cfa480285
Lito <lito@eordes.com>
2013-04-29 20:18:27 +0200

    Added (again) composer support

---
+++ phpcan/libs/clases-namespaces.php
@@ -0,0 +1,17 @@
+<?php
+defined('ANS') or die();
+
+return array(
+    'classes' => array(
+        'PHPMailer' => __DIR__.'/phpmailer/phpmailer/class.phpmailer.php',
+        'POP3' => __DIR__.'/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__.'/phpmailer/phpmailer/class.smtp.php',
+        'phpmailerException' => __DIR__.'/phpmailer/phpmailer/class.phpmailer.php'
+    ),
+    'namespaces' => array(
+        'Stylecow' => __DIR__.'/stylecow/stylecow',
+        'Imagecow' => __DIR__.'/imagecow/imagecow',
+        'Faker' => __DIR__.'/fzaninotto/faker/src',
+        'ANS\\Cache' => __DIR__.'/ANS/Cache/libs'
+    )
+);
