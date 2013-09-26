738484fa658d923731ce6c15b2803e0afedeeb62
Lito <lito@eordes.com>
2013-09-26 11:30:43 +0200

    Fixed CSS load

--- phpcan/libs/ANS/PHPCan/Files/Css/Css.php
+++ phpcan/libs/ANS/PHPCan/Files/Css/Css.php
@@ -101,9 +101,9 @@ class Css {
         return $this;
     }
 
-    public function toString (array $options = array())
+    public function toString ($options = array())
     {
-        $css = $this->Css->toString($options);
+        $css = $this->Css->toString((array)$options);
 
         return ($this->settings['cache']['minify'] ? $this->minify($css) : $css);
     }
