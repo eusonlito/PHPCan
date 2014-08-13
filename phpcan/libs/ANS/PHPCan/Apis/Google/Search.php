856e81a2fde6d46b8dd466da5c1d11ec1f1967d7
Lito <lito@eordes.com>
2014-08-13 11:12:33 +0200

    Improved Google Search API

--- phpcan/libs/ANS/PHPCan/Apis/Google/Search.php
+++ phpcan/libs/ANS/PHPCan/Apis/Google/Search.php
@@ -17,23 +17,65 @@ use \ANS\PHPCan\Apis;
 
 class Search extends \ANS\PHPCan\Apis\Api
 {
+    private $result = [];
+
     /**
-     * public function search (string/array $data, [int $cache], [boolean $return_array])
+     * public function search (string/array $data, [int $cache])
      *
      * return array
      */
-    public function search ($data, $cache = null, $return_array = true)
+    public function search($data, $cache = null)
     {
         if (is_string($data)) {
             $data = array('q' => $data);
         }
 
         $data['v'] = '1.0';
+        $data['rsz'] = empty($data['rsz']) ? '8' : $data['rsz'];
+        $data['start'] = empty($data['start']) ? '1' : $data['start'];
+        $data['ip'] = ip();
 
-        $return = $this->getJSON('http://ajax.googleapis.com/ajax/services/search/web', $data, $cache);
+        $this->result = $this->getJSON('http://ajax.googleapis.com/ajax/services/search/web', $data, $cache);
 
-        if ($return_array) {
-            return $this->toArray($return);
+        return $this;
+    }
+
+    public function getRaw()
+    {
+        return $this->toArray($this->result);
+    }
+
+    public function getSimple()
+    {
+        if (empty($this->result) || empty($this->result->responseData->results)) {
+            return array(
+                'results' => array(),
+                'pagination' => array()
+            );
         }
+
+        $simple = array();
+
+        foreach ($this->result->responseData->results as $value) {
+                $simple['results'][] = array(
+                    'url' => $value->unescapedUrl,
+                    'escaped_url' => $value->url,
+                    'domain' => $value->visibleUrl,
+                    'cache' => $value->cacheUrl,
+                    'formated_title' => $value->title,
+                    'title' => $value->titleNoFormatting,
+                    'text' => $value->content
+                );
+        }
+
+        $simple['pagination'] = array(
+            'first' => 1,
+            'last' => count($this->result->responseData->cursor->pages),
+            'total_pages' => count($this->result->responseData->cursor->pages),
+            'total' => $this->result->responseData->cursor->estimatedResultCount,
+            'page' => $this->result->responseData->cursor->currentPageIndex + 1
+        );
+
+        return $simple;
     }
 }
